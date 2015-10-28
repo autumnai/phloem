62df52bddaf4018a11261a0f0e4d19b9542bf8fd
MichaelHirn <michael.hirn@storeness.de>
2015-10-28 13:41:31 +0100

    feat(blob): add Numeric trait for Blob generics

--- tests/blob_spec.rs
+++ tests/blob_spec.rs
@@ -6,22 +6,32 @@ mod blod_spec {
     use phloem::Blob;
 
     #[test]
-    fn construction_basic() {
-        let blob: Blob = Blob::new();
+    fn it_works_for_f32() {
+        let _: Blob<f32> = Blob::new();
+    }
+
+    #[test]
+    fn it_works_for_f64() {
+        let _: Blob<f64> = Blob::new();
+    }
+
+    #[test]
+    fn it_works_basic_construction() {
+        let blob: Blob<f32> = Blob::new();
         assert_eq!(blob.cpu_data().capacity(), 0);
     }
 
     #[test]
-    fn construction_of_shape() {
+    fn it_works_of_shape_construction() {
         let shape = vec![2, 3, 2];
-        let blob: Blob = Blob::of_shape(shape);
+        let blob: Blob<f32> = Blob::of_shape(shape);
         assert_eq!(12, blob.cpu_data().capacity());
     }
 
     #[test]
-    fn shape_string() {
+    fn it_works_shape_string() {
         let shape = vec![2, 3, 2];
-        let blob: Blob = Blob::of_shape(shape);
+        let blob: Blob<f32> = Blob::of_shape(shape);
         assert_eq!("2 3 2 (3)", blob.shape_string());
     }
 }
